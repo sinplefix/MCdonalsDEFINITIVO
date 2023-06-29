39f1109dd714ecf506371e27d404900d26a2fc94
Benjamin <114114209+sinplefix@users.noreply.github.com>
2023-06-29 15:49:02 -0400

    oso aqui esta

---
+++ addimages.py
@@ -0,0 +1,26 @@
+import cx_Oracle
+
+
+conn = cx_Oracle.connect("inacap_ecommerce/.Inacap.2023..@db20220530152721_high")
+# connection = cx_Oracle.connect("inacap_ecommerce/.Inacap.2023..@nombre_de_servicio")
+
+
+with open("bigmac.png", "rb") as file:
+    imagen_data = file.read()
+
+
+
+
+cursor = conn.cursor()
+cursor.execute("INSERT INTO MCDONALDS2 VALUES (:id, :nombre,:descripcion,:precio, :imagen)",
+               id=1, nombre="bigmac.png",descripcion='Deliciosa hamburguesa con queso fundido',precio =4.99, imagen=imagen_data)
+conn.commit()
+
+
+
+cursor.close()
+conn.close()
+
+print("Imagen insertada correctamente")
+
+
---
+++ coneccion.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+    fetch('https://g9cd7530b8a8613-ecommerce.adb.sa-santiago-1.oraclecloudapps.com/ords/inacap_ecommerce/mcdonalds1/')
+      .then((resultado) => {
+        return resultado.json();
+      })
+      .then((datos) => {
+        console.log(datos);
+        for (let i in datos.items) {
+          console.log(datos.items[i]);
+          let imagenURL = `img/${datos.items[i].id}.png`; // Ruta de la imagen en el servidor
+          document.getElementById('productos1').innerHTML += `
+            <div class="col">
+              <div class="card" style="width: 18rem;">
+                <img src="${imagenURL}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <ul>
+                    <strong>ID</strong>: ${datos.items[i].id}
+                    <li><strong>Nombre</strong>: ${datos.items[i].nombre}</li>
+                    <li><strong>Descripcion</strong>: ${datos.items[i].descripcion}</li>
+                    <li><strong>Precio $</strong>: ${datos.items[i].precio}</li> <!-- ETIQUETA OFICIAL --> 
+                  </ul>
+                  <div class="card-footer ">
+                    <a href="#" class="btn btn-success justify-content-start">Añadir</a>
+                    <a href="#" class="btn btn-danger justify-content-end">Quitar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
+        }
+      })
+      .catch(() => {});
+  });
+  
